3878689a6cb54a0ac479e1c0458efec413f5e747
nicolasbuenoozz <161485181+nicolasbuenoozz@users.noreply.github.com>
2025-09-23 08:51:13 -0300

    codigos angelo

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import flask
+from flask import Flask, render_template
 app= flask (__name__)
-
 @app.route('/')
 def hello_world
-     return 'hello, World!'
-     if__name__=='__main__':
-        app.run(debug=True)
+     return render_template ('index.html')
+   
+@app.route('/login',methods=['GET','POST'])
+def login():
+   error: None
+   if request.method == 'POST':
+      username = request.form['username']
+      password = request.form['password']
+      if username == 'admin' and password == 'password':
+         return 'Login com sucesso'
+     else:
+      error = 'Credenciais invalidas. Tente novamente.'
+   return render_template('login.html', error=error)
+
+if__name__=='__main__':
+   port = int(os.environ.get('PORT',5000))
+   app.run(host= '0.0.0.0', port=port, debug=True)
