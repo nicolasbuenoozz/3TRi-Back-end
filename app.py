8c610e4f8ad1a372335961e575c922e473066693
nicolasbuenoozz <161485181+nicolasbuenoozz@users.noreply.github.com>
2025-10-02 07:52:31 -0300

    bakck-end 02/10

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-from flask import Flask, render_template
-app= flask (__name__)
+from flask import Flask, render_template  # Adicione render_template
+app = Flask(__name__)
 @app.route('/')
-def hello_world
-     return render_template ('index.html')
-   
-@app.route('/login',methods=['GET','POST'])
+def hello_world():
+       return render_template('index.html')
+       
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-   error: None
-   if request.method == 'POST':
-      username = request.form['username']
-      password = request.form['password']
-      if username == 'admin' and password == 'password':
-         return 'Login com sucesso'
-     else:
-      error = 'Credenciais invalidas. Tente novamente.'
-   return render_template('login.html', error=error)
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']  # Removida a vírgula desnecessária
+        password = request.form['password']
+        if username == 'admin' and password == 'password':
+            return 'Login com sucesso'  # Redireciona para home em sucesso
+        else:
+            error = 'Credenciais inválidas. Tente novamente.'
+    return render_template('login.html', error=error)
 
-if__name__=='__main__':
-   port = int(os.environ.get('PORT',5000))
-   app.run(host= '0.0.0.0', port=port, debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
